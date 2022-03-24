5ef93276886c6933907225f8d8f3a47ad899113b
joodeci <58897838+joodeci@users.noreply.github.com>
2022-03-24 03:21:15 -0700

    Update about page
    Add new position

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,9 +18,9 @@ export default class BlogIndexPage extends React.Component {
               <div className="about--content">
                 <h1>I'm Jodeci Correa. Product designer and occasional American R&B quartet</h1>
                 <h5>My work combines a background in graphic design, along with technology and user focused design thinking. I believe in technology and design as a means to connect and shape how humans around the world are seen, heard, and felt. My ethos in life is to always improve and leave things better than the way I found them.</h5>
-                <p>Currently, I'm a product designer on the Subscriptions team at <a href="https://boldcommerce.com/">Bold Commerce.</a> I help design and build world class applications for e-commerce platforms such as Shopify, BigCommerce, and WooCommerce. During my time here I have been responsible for all phases of the design process, from spearheading design sprints, leading design thinking workshops, conducting user research and testing, to wireframing and prototyping high fidelity mock ups.</p>
-                <p>Before that, I studied graphic design at Red River College in Winnipeg, MB. <a href="https://www.figma.com/file/83Z1rCwQJsEWBGnBRbexac/Resume?node-id=2%3A2">Check out my resume to see more details about my ever growing career as a designer.</a></p>
-                <p>Outside of work, I'm a casual photographer and avid collector of action figures and sneakers. Consumer of many music genres and self proclaimed movie buff, <a href="https://letterboxd.com/jodeci/">here's a snapshot of my movie taste.</a></p>
+                <p>Currently, I'm a product designer @ <a href="https://www.article.com/">Article.</a></p>
+                <p>Previously, I studied graphic design at Red River College in Winnipeg, MB. <a href="https://www.figma.com/file/83Z1rCwQJsEWBGnBRbexac/Resume?node-id=2%3A2">Check out my resume to see more details about my ever growing career as a designer.</a></p>
+                <p>Outside of work, I'm a casual photographer and avid collector of action figures and sneakers. Consumer of many music genres and self proclaimed <a href="https://letterboxd.com/jodeci/">movie buff.</a></p>
               </div>
             </div>
 
